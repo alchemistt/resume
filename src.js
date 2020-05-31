8f07f0b34196d73e480127c5662867942aa9873a
alchemistt <tuhsarbudhiraja01@gmail.com>
2020-05-31 22:33:48 +0530

    fixed nav linkdin github

--- src.js
+++ src.js
@@ -16,4 +16,9 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-document.getElementById('skillset').scrollIntoView();
+
+
+function skillsetgit() {
+    var elmnt = document.getElementById("skillset");
+    elmnt.scrollIntoView();
+}
